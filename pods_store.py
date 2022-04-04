1a144b2218e3fe0e52e707007e7b648ddef83d9b
NevilleJS <69287021+s3dman@users.noreply.github.com>
2022-04-04 12:34:00 +0100

    pods store get query and remove query done

--- pods_store.py
+++ pods_store.py
@@ -10,3 +10,13 @@ class PodStore:
             "cpu_required": cpu_required,
             "assigned_node": assigned_node,
         }
+
+    def pod_exists(self, pod_id):
+        return pod_id in self.pods
+
+    def get_all_pods(self):
+        return self.pods
+
+    def remove_pod(self, pod_id):
+        if pod_id in self.pods:
+            del self.pods[pod_id]
