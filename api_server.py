f7b681a96e52088a311d317c0d49217794f2b22d
nevin <nevinmarthandan72@gmail.com>
2022-04-05 12:34:00 +0100

    default route.

--- api_server.py
+++ api_server.py
@@ -5,3 +5,13 @@ import uuid
 from flask import Flask, request, jsonify
 from node_store import NodeStore
 from pods_store import PodStore
+
+app = Flask(__name__)
+
+store = NodeStore()
+pod_store = PodStore()
+
+
+@app.route("/")
+def home():
+    return "Distributed Cluster Simulation API Running"
