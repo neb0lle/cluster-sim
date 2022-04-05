2302ffabcad6c32cbeee9739ab175625b2befb4c
nevin <nevinmarthandan72@gmail.com>
2022-04-05 12:34:00 +0100

    register pods fix.

--- api_server.py
+++ api_server.py
@@ -15,3 +15,48 @@ pod_store = PodStore()
 @app.route("/")
 def home():
     return "Distributed Cluster Simulation API Running"
+
+
+# Launch a new Docker container simulating a node
+def launch_node_container(node_id, cpu_cores):
+    try:
+        subprocess.run(
+            [
+                "docker",
+                "run",
+                "-d",
+                "--name",
+                node_id,
+                "-e",
+                f"NODE_ID={node_id}",
+                "-e",
+                f"CPU_CORES={cpu_cores}",
+                "-e",
+                "API_SERVER=http://host.docker.internal:5000",
+                "cluster-node",
+            ],
+            check=True,
+        )
+        print(f"[+] Launched container for node {node_id}")
+    except subprocess.CalledProcessError as e:
+        print(f"[!] Failed to launch container: {e}")
+
+
+# Register a node and launch its container
+@app.route("/register_node", methods=["POST"])
+def register_node():
+    data = request.get_json()
+    node_id = data.get("node_id")
+    cpu_cores = data.get("cpu_cores")
+
+    if not node_id or cpu_cores is None:
+        return jsonify({"error": "Missing node_id or cpu_cores"}), 400
+
+    success = store.register_node(node_id, cpu_cores)
+    if success:
+        launch_node_container(node_id, cpu_cores)
+        return jsonify(
+            {"message": f"Node '{node_id}' registered and container launched."}
+        ), 200
+    else:
+        return jsonify({"message": f"Node '{node_id}' already exists."}), 409
