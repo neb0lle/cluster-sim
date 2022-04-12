83bbb7bc5fb29eae7f2b968663139f00c0d3b5d5
arfa <thearfa99@gmail.com>
2022-04-12 12:34:00 +0100

    pods reschedule added

--- node_store.py
+++ node_store.py
@@ -34,3 +34,42 @@ class NodeStore:
                     print(f"[!] Node {node_id} marked as unhealthy.")
                     self.nodes[node_id]["status"] = "unhealthy"
                     self.reschedule_pods(node_id, pod_store)
+
+    def reschedule_pods(self, failed_node_id, pod_store):
+        failed_pods = self.nodes[failed_node_id]["pods"]
+        self.nodes[failed_node_id]["pods"] = []
+        self.nodes[failed_node_id]["available_cpu"] = self.nodes[failed_node_id][
+            "cpu_cores"
+        ]
+
+        for pod_id in failed_pods:
+            cpu_required = pod_store.pods[pod_id]["cpu_required"]
+            reassigned = False
+
+            for node_id, info in self.nodes.items():
+                if (
+                    info["status"] == "healthy"
+                    and info["available_cpu"] >= cpu_required
+                ):
+                    info["pods"].append(pod_id)
+                    info["available_cpu"] -= cpu_required
+                    pod_store.pods[pod_id]["assigned_node"] = node_id
+                    print(f"[+] Pod {pod_id} rescheduled to {node_id}")
+                    reassigned = True
+                    break
+
+            if not reassigned:
+                print(f"[!] Pod {pod_id} could not be rescheduled.")
+
+    def schedule_pod(self, pod_id, cpu_required):
+        # Simple First-Fit strategy
+        for node_id, info in self.nodes.items():
+            if info["status"] == "healthy" and info["available_cpu"] >= cpu_required:
+                info["pods"].append(pod_id)
+                info["available_cpu"] -= cpu_required
+                return {"assigned_node": node_id, "cpu_used": cpu_required}
+        return None
+
+    def get_all_nodes(self, pod_store):
+        self.check_node_health(pod_store)  # ✅ pass pod_store to health check
+        return self.nodes
